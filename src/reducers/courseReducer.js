5e93a1811f8a9220ac54dcde0ab2ec64116d7f2c
Arsh <arsh.hunjan@gmail.com>
2018-01-13 23:39:35 -0500

    Added Redux actions , reducer, store

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,12 @@
+// state = [] initialize empty state
+export default function courseReducer(state = [], action) {
+    switch(action.type) {
+        case 'CREATE_COURSE' :
+            return [...state,
+                Object.assign({}, action.course)
+            ];
+        
+        default: 
+            return state;
+    }
+} 
