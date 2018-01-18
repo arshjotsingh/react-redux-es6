655504c96214168a44d2e850a76f7da3ed0b447c
Arsh <arsh.hunjan@gmail.com>
2018-01-17 21:42:44 -0500

    Write in Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-    courses //shorthand prop name
+    courses, //shorthand prop name
+    authors
 });
 
 export default rootReducer;
